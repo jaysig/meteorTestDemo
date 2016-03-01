0a45ced9b460ffb6955855766576d091ad059094
Jaysig <jasontsigmon@gmail.com>
2016-03-01 11:48:34 -0500

    added steps

--- tests/features/create-widget.feature
+++ tests/features/create-widget.feature
@@ -1,16 +1,15 @@
-Feature: Create a Widget
 #User story used to explain what is going on
 # Based on http://www.mhurwi.com/tutorial-test-driven-development-with-meteor-cucumber-chimp/
+Feature: Create a Widget
   As a visitor to the site,
   so that I can manage widgets,
   I want to create and delete widgets on my homepage.
   
-
+  	#steps taken before every scenario
     Background:
-        #steps taken before every scenario
         Given I am on the site
 
-
+    @watch
     Scenario: Visitor creates a widget
         #what we are testing
         When I name a widget "alpha"
--- tests/features/create-widget_steps.js
+++ tests/features/create-widget_steps.js
@@ -1,5 +1,23 @@
-module.exports = function () {
-	'use strict';
+module.exports = function() {  
+ 'use strict';
 
-	// steps go here
+ this.Given(/^I am on the site$/, function () {
+      // Write the automation code here
+      pending();
+ });
+
+ this.When(/^I name a widget "([^"]*)"$/, function (arg1) {
+   ‘// Write the automation code here
+   pending();
+ });
+
+ this.When(/^submit the form$/, function () {
+       // Write the automation code here
+       pending();
+ });
+
+ this.Then(/^I should see a list of widget names containing "([^"]*)"$/, function (arg1) {
+      // Write the automation code here
+   ’pending();
+ });
 };
