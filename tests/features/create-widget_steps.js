44d68922fea3b11f8536e68ba5e362d2d1fed8a3
Jaysig <jasontsigmon@gmail.com>
2016-03-01 11:56:46 -0500

    finished steps, changed HTML

--- tests/features/create-widget_steps.js
+++ tests/features/create-widget_steps.js
@@ -2,22 +2,20 @@ module.exports = function() {
  'use strict';
 
  this.Given(/^I am on the site$/, function () {
-      // Write the automation code here
-      pending();
+      browser.url('http://localhost:3000');
  });
 
  this.When(/^I name a widget "([^"]*)"$/, function (arg1) {
-   ‘// Write the automation code here
-   pending();
+   browser.setValue('input[name="widgetName"]', arg1);
  });
 
  this.When(/^submit the form$/, function () {
-       // Write the automation code here
-       pending();
+       browser.click('input[type="submit"]');
  });
 
  this.Then(/^I should see a list of widget names containing "([^"]*)"$/, function (arg1) {
-      // Write the automation code here
-   ’pending();
+      let _el = '.widget-name';
+      browser.waitForVisible(_el);
+      expect(browser.getText(_el)).toEqual(arg1);
  });
 };
